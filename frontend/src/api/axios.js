0fc034b430a2355d96fbb9434f4b8dfe821ab8ca
Abhishek Sardar <139051454+AbhishekSardar05@users.noreply.github.com>
2026-05-12 11:07:48 +0530

    df

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'https://skilllens-dpn0.onrender.com/api',
+  baseURL: 'https://skilllens-1.onrender.com/api',
   withCredentials: true
 })
 
